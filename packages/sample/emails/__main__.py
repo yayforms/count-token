20e9c91d349376b8e11196ef3e3a423e38c46e9a
Rafael Girao <rgiraon@gmail.com>
2023-03-28 09:40:23 -0400

    .

--- packages/sample/emails/__main__.py
+++ packages/sample/emails/__main__.py
@@ -1,8 +1,6 @@
 import tiktoken
 
 def main(args):
-    enc = tiktoken.get_encoding("cl100k_base")
-    assert enc.decode(enc.encode("hello world")) == "hello world"
     return {
         "statusCode": 200,
         "body": "no twilio verified phone numbers provided"
