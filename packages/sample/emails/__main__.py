fd9b9cf020f75713560e5ac2c302ceebf095b0c2
Rafael Girao <rgiraon@gmail.com>
2023-03-28 09:45:40 -0400

    .

--- packages/sample/emails/__main__.py
+++ packages/sample/emails/__main__.py
@@ -1,7 +1,15 @@
 import tiktoken
 
+def num_tokens_from_string(string: str, encoding_name: str) -> int:
+    """Returns the number of tokens in a text string."""
+    encoding = tiktoken.get_encoding(encoding_name)
+    num_tokens = len(encoding.encode(string))
+    return num_tokens
+
 def main(args):
+    tokens = num_tokens_from_string("tiktoken is great!", "cl100k_base")
+
     return {
         "statusCode": 200,
-        "body": "no twilio verified phone numbers provided"
+        "body": tokens
     }
